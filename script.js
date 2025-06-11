fe44782860c040f318222873b93ed47d42e65cfe
kgdee <kgdee7603@gmail.com>
2025-06-11 00:00:59 -0400

    commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+const input = document.querySelector(".input");
 
-const input = document.querySelector(".input")
-
-function sanitize() { 
+function sanitize() {
   let sanitized = input.value.replace(/[<>:"/\\|?*]/g, "").replace(/\./g, " ");
   document.querySelector(".output").innerHTML = `Filename sanitized and copied!<br/> ${sanitized}`;
 
-  navigator.clipboard.writeText(sanitized)
+  navigator.clipboard.writeText(sanitized);
 }
