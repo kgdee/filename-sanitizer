3a3eb6b739a5083ffcfc858c2a761ba684212d71
kgdee <kgdee7603@gmail.com>
2025-08-27 06:09:33 -0400

    commit

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const textInput = document.querySelector(".text-input");
-const messageEl = document.querySelector(".message")
+const messageEl = document.querySelector(".message");
 
 function sanitize() {
-  const text = textInput.value
-  if (!text) return
+  const text = textInput.value;
+  if (!text) return;
 
   let sanitized = text
-    .replace(/[<>:"/\\|?*\x00-\x1F]/g, " ")
-    .trim()
-    .replace(/\s+/g, " ");
+    .replace(/[:|]/g, " -")
+    .replace(/["']/g, "")
+    .replace(/\s*-\s*/g, " - ")
+    .replace(/[<>\/\\|?*\x00-\x1F]/g, "")
+    .replace(/\s+/g, " ")
+    .trim();
 
   messageEl.innerHTML = `Filename sanitized and copied!<br/> <span>${sanitized}</span>`;
 
   navigator.clipboard.writeText(sanitized);
 }
-
-document.addEventListener("keydown", function(event) {
-  if (event.key === " ") {
-    event.preventDefault()
-    sanitize()
-  }
-})
